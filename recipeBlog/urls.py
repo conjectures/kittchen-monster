c4ac7002c8a3d10170b2657acc7e8abb19d39b1c
alex <alexis.othonos@gmail.com>
2020-11-07 18:31:03 +0000

    add post create edit delete views

--- recipeBlog/urls.py
+++ recipeBlog/urls.py
@@ -6,4 +6,7 @@ from django.urls import path
 urlpatterns = [
         path('', views.HomeView.as_view(), name='home'),
         path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name='recipe_detail'),
+        path('recipe/create/', views.RecipeCreateView.as_view(), name='recipe_create'),
+        path('recipe/<int:pk>/edit/', views.RecipeUpdateView.as_view(), name='recipe_edit'),
+        path('recipe/<int:pk>/delete/', views.RecipeUpdateView.as_view(), name='recipe_delete'),
         ]
--- recipeBlog/views.py
+++ recipeBlog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from .models import Post
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
@@ -12,3 +13,18 @@ class HomeView(ListView):
 class RecipeDetailView(DetailView):
     model = Post
     template_name = 'recipe_detail.html'
+
+
+class RecipeCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+
+
+class RecipeUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+
+
+class RecipeDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('home')
