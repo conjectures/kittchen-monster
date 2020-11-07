e74a835cd98fedbeb17fbe707c640d4ee5712a65
alex <alexis.othonos@gmail.com>
2020-11-07 08:36:37 +0000

    added inital home page

--- kittchen/urls.py
+++ kittchen/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-fromdjango.views.generic import RedirectView
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('recipeBlog.urls')),
     path('', RedirectView.as_view(url='home/', permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpattenrs = [
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-        ]
