7fa0fd1df5f6f3f3ddceb6eb1e567d17bd99f27f
alex <alexis.othonos@gmail.com>
2020-11-07 11:02:57 +0000

    added bootstrap, models

--- recipeBlog/admin.py
+++ recipeBlog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+from django.contrib.auth.models import User
+
+admin.site.register(Post)
--- recipeBlog/models.py
+++ recipeBlog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Post
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # delete blogs of deleted user
+    # ingredients = models.ManyToManyField(Ingredients, help_text='Input ingredients')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + ' by ' + self.author
+
+
+
+
